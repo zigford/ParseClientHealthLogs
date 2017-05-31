73ea83b5014cc5dc0ad62aa34cb58fb0a179125e
Jesse Harris <jpharris@usc.edu.au>
2017-05-31 17:16:21 +1000

    Initial upload of script

---
+++ Scripts/ParseClientHealth.psm1
@@ -0,0 +1,28 @@
+function Get-ClientHealthLogs {
+[CmdLetBinding()]
+Param($Path="C:\Jesse\Logs")
+
+    $Logs = Get-ChildItem -Path $Path -Recurse -Filter *.log
+    Write-Verbose "$($Logs.Count) logs found"
+    ForEach ($Log in $Logs) {
+        Write-Verbose "Reading $Log"
+        $LogContent = Get-Content -Path $Log.FullName
+        $LogObject = New-Object -TypeName psobject
+        $LogContent | ForEach-Object {
+            $_ -match '^(?<key>\w{1,}):\s(?<value>.*$)' | Out-Null
+            If ($Matches) {
+                Write-Verbose "Found match on $($Matches['key'])"
+                $LogObject | Add-Member `
+                    -MemberType NoteProperty `
+                    -Name $Matches['key'] `
+                    -Value $Matches['value'] -Force
+                If ($Matches['key'] -eq 'Timestamp') {
+                    $LogObject 
+                    $LogObject = New-Object -TypeName psobject
+                    $Matches = $null
+                }   
+            }
+
+        }
+    }
+}
